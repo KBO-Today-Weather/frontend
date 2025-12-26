9de0ee066dd1c3b7fa7850ca82d10e212d5c09f2
임아정 <ajeong@thewc.co.kr>
2025-12-26 15:56:28 +0900

    chore: typography 컴포넌트 정의

--- src/components/ui/pageHeader.tsx
+++ src/components/ui/pageHeader.tsx
@@ -1,8 +1,10 @@
+import { H3, P } from "@/lib/Typography";
+
 const PageHeader = ({ title, description }: { title: string, description?: string }) => {
   return (
     <div className="gap-2 grid">
-      <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">{title}</h3>
-      <p className="text-sm text-gray-500">{description}</p>
+      <H3>{title}</H3>
+      <P className="text-gray-500">{description}</P>
     </div>
   );
 };
